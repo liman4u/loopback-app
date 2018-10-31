064bf0685903c2be21f6b8e87b12a56426527bfd
Liman Adamu Labaran <limanadamu@172-6-3-30.lightspeed.irvnca.sbcglobal.net>
2018-10-31 11:39:21 +0000

    Adds a remote hook for additional cat paramater description

--- common/models/cat.js
+++ common/models/cat.js
@@ -2,6 +2,12 @@
 
 module.exports = function(Cat) {
 
+    Cat.afterRemote('findById', function(context,cat,next){
+        cat.description = cat.name + " is "+ cat.age + " years old  and is a " + cat.breed;
+
+        next();
+    });
+
     Cat.adoptable = function(id,cb){
         Cat.findById(id,function(err, cat){
             if(err) return cb("Error",null);
