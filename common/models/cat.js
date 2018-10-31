e9701904de791ebc4b96eb5d6129898692e45cbc
Liman Adamu Labaran <limanadamu@172-6-3-30.lightspeed.irvnca.sbcglobal.net>
2018-10-31 11:26:00 +0000

    Adds a remote method adoptable

--- common/models/cat.js
+++ common/models/cat.js
@@ -2,4 +2,23 @@
 
 module.exports = function(Cat) {
 
+    Cat.adoptable = function(id,cb){
+        Cat.findById(id,function(err, cat){
+            if(err) return cb("Error",null);
+
+            if(!cat) return cb("Cat not found",null);
+            let canAdopt = false;
+
+            if(cat.breed != "tiger" || (cat.age >= 10) ) canAdopt = true;
+
+            cb(null,canAdopt);
+
+        });
+    }
+
+    Cat.remoteMethod('adoptable',{
+        accepts: {arg: 'id' , type: 'any'},
+        returns: {arg: 'adoptable', type: 'boolean'}
+    });
+
 };
